f2002f61ae5e59dcc9fc51eaaebc86ca022f8262
fermurbra <fermurbra@alum.us.es>
2026-03-01 10:38:30 +0100

    fix: usar ModuleProvider() para registrar notification provider correctamente en Awilix container

--- backend/src/modules/email-notifications/index.ts
+++ backend/src/modules/email-notifications/index.ts
@@ -1,10 +1,8 @@
-import { ModuleProviderExports } from '@medusajs/framework/types'
+import { ModuleProvider, Modules } from '@medusajs/framework/utils'
 import { ResendNotificationService } from './services/resend'
 
 const services = [ResendNotificationService]
 
-const providerExport: ModuleProviderExports = {
+export default ModuleProvider(Modules.NOTIFICATION, {
   services,
-}
-
-export default providerExport
+})
